3048c0c9c7db932427293a34dc90b63b65a678e0
antdev <antdev1987@gmail.com>
2024-03-17 20:36:15 -0600

    dia fixed format

--- src/utils/formatDateSinHora.js
+++ src/utils/formatDateSinHora.js
@@ -2,7 +2,7 @@ function formatDateSinHora(date) {
     let fecha = new Date(date);
     let año = fecha.getFullYear();
     let mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
-    let dia = ("0" + fecha.getDate()).slice(-2);
+    let dia = ("0" + (fecha.getDate() + 1)).slice(-2);
     // let horas = ("0" + fecha.getHours()).slice(-2);
     // let minutos = ("0" + fecha.getMinutes()).slice(-2);
     // let segundos = ("0" + fecha.getSeconds()).slice(-2);
